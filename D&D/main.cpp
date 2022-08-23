32e309eb033f2d24f6d2d0830cf9f9fd51ff4d8a
Yusuf Utkurak <yusufmacut@icloud.com>
2022-08-23 11:45:19 +0300

    Initial Commit

---
+++ D&D/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  D&D
+//
+//  Created by yusuf utkurak on 23.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
